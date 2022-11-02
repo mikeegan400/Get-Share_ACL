c89adead846f5340b02e278cfc2f29c57aec2ae2
Mike Egan <74567500+mikeegan400@users.noreply.github.com>
2022-11-02 12:35:42 -0500

    Add files via upload

---
+++ Get-Share_ACL.ps1
@@ -0,0 +1,5 @@
+﻿$shares = get-smbshare | where-object {$_.Path -like "*F:\*" -and $_.Path -ne "F:\"} | select-object Path
+foreach ($share in $shares)
+{
+	(get-acl -name $share.path).access | where-object {$_.IdentityReference -like "*fishing*"} | select-object IdentityReference,objectclass
+}
