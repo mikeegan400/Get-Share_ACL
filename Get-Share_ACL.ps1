d1b4dabfdcd09e9a86f881a647ad8a921b45e4ed
Mike Egan <74567500+mikeegan400@users.noreply.github.com>
2022-11-09 06:55:17 -0600

    Update Get-Share_ACL.ps1
    Corrected the get-acl statement.  The switch should be -path, not -name

--- Get-Share_ACL.ps1
+++ Get-Share_ACL.ps1
@@ -1,5 +1,5 @@
 ﻿$shares = get-smbshare | where-object {$_.Path -like "*F:\*" -and $_.Path -ne "F:\"} | select-object Path
 foreach ($share in $shares)
 {
-	(get-acl -name $share.path).access | where-object {$_.IdentityReference -like "*fishing*"} | select-object IdentityReference,objectclass
+	(get-acl -path $share.path).access | where-object {$_.IdentityReference -like "*fishing*"} | select-object IdentityReference,objectclass
 }
